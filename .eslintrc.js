030aea0c1572ef538af8eab1e6786dbc5c749b85
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-30 13:06:34 +0200

    Installing Eslint and eslint-config-prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
