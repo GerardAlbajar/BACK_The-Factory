e526e5be763dae5a6595d8848e175c55f3f3d811
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-30 17:42:28 +0200

    Adding userRegisterSchema at schemas/userSchema.js

--- src/server/schemas/userSchema.js
+++ src/server/schemas/userSchema.js
@@ -13,4 +13,25 @@ const userLoginSchema = {
   }),
 };
 
-module.exports = { userLoginSchema };
+const userRegisterSchema = {
+  body: Joi.object({
+    name: Joi.string()
+      .max(20)
+      .messages({ message: "A name is required" })
+      .required(),
+    mail: Joi.string()
+      .max(20)
+      .messages({ message: "An email is required" })
+      .required(),
+    username: Joi.string()
+      .max(20)
+      .messages({ message: "A username is required" })
+      .required(),
+    password: Joi.string()
+      .max(20)
+      .messages({ message: "A password is required" })
+      .required(),
+  }),
+};
+
+module.exports = { userLoginSchema, userRegisterSchema };
