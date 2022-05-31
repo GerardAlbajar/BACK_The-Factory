7f6af9274962d35b053213472daddf9287cc40f8
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-31 16:25:37 +0200

    Updating cors options at server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -5,23 +5,9 @@ const cors = require("cors");
 const { notFoundError, generalError } = require("./middlewares/errors");
 const usersRouters = require("./routers/userRouters");
 
-const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:3002",
-    "http://localhost:4000",
-    "http://localhost:4001",
-    "http://localhost:4002",
-    "http://localhost:5000",
-    "http://localhost:5001",
-    "http://localhost:5002",
-  ],
-};
-
 const app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
