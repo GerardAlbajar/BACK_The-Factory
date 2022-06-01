a0ab016cd9d68e2886bb7cea3bda0d669dd00457
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-01 13:39:33 +0200

    Testing if getAstroParts function calls its responses method with 200

---
+++ src/server/controllers/astroPartController.test.js
@@ -0,0 +1,23 @@
+const { mockAstroParts } = require("../../mocks/astroParts");
+const { getAstroParts } = require("./astroPartController");
+
+jest.mock("../../database/models/AstroPart", () => ({
+  ...jest.requireActual("../../database/models/AstroPart"),
+  find: jest.fn().mockResolvedValue(mockAstroParts),
+}));
+
+describe("Given a getAstroParts function", () => {
+  describe("When invoked when a response and a list of AstroParts", () => {
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    test("Then it should call the response status method with 200", async () => {
+      const expectedStatus = 200;
+
+      await getAstroParts(null, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+  });
+});
