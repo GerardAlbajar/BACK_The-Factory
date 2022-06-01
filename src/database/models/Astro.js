bfbf61f10decd55e8724288b2133ee8e9af1a89a
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-01 12:55:04 +0200

    Adding Astro.js schema at database/models and astros.json

---
+++ src/database/models/Astro.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require("mongoose");
+
+const AstroSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  flighthistory: {
+    type: Boolean,
+    required: true,
+  },
+  stickers: {
+    type: Number,
+    required: true,
+  },
+  framework: {
+    type: String,
+  },
+  assembled: {
+    type: Boolean,
+    required: true,
+  },
+  parts: {
+    rocket: {
+      type: Schema.Types.ObjectId,
+      ref: "AstroPart",
+      required: true,
+    },
+    astro: {
+      type: Schema.Types.ObjectId,
+      ref: "AstroPart",
+      required: true,
+    },
+    naut: {
+      type: Schema.Types.ObjectId,
+      ref: "AstroPart",
+      required: true,
+    },
+  },
+  owner: {
+    type: Schema.Types.ObjectId.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Astro = model("Astro", AstroSchema, "astros");
+
+module.exports = Astro;
