7e0dd2c9f8e28894fce4425b8f6bc171cabc286b
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-01 16:07:10 +0200

    Testing GET '/astros' endpoint at astroRouters.test.js

---
+++ src/mocks/astros.js
@@ -0,0 +1,62 @@
+const mockAstros = [
+  {
+    id: "#612736SJDGHA",
+    name: "REACT PERFECT ASTRO",
+    type: "Perfect Astro",
+    flighthistory: "false",
+    stickers: "1",
+    framework: "React",
+    assembled: "false",
+    parts: {
+      rocket: "62974453ec4b32f200362330",
+      astro: "62974453ec4b32f20036232e",
+      naut: "62974453ec4b32f20036232f",
+    },
+  },
+  {
+    id: "#612736SJDGHB",
+    name: "VUE PERFECT ASTRO",
+    type: "Perfect Astro",
+    flighthistory: "false",
+    stickers: "1",
+    framework: "Vue",
+    assembled: "false",
+    parts: {
+      rocket: "62974453ec4b32f200362333",
+      astro: "62974453ec4b32f200362331",
+      naut: "62974453ec4b32f200362332",
+    },
+  },
+  {
+    id: "#612736SJDGHC",
+    name: "ANGULAR PERFECT ASTRO",
+    type: "Perfect Astro",
+    flighthistory: "false",
+    stickers: "1",
+    framework: "Angular",
+    assembled: "false",
+    parts: {
+      rocket: "62974453ec4b32f200362336",
+      astro: "62974453ec4b32f200362334",
+      naut: "62974453ec4b32f200362335",
+    },
+  },
+  {
+    id: "#612736SJDGHD",
+    name: "JQUERY PERFECT ASTRO",
+    type: "Perfect Astro",
+    flighthistory: "false",
+    stickers: "1",
+    framework: "JQuery",
+    assembled: "false",
+    parts: {
+      rocket: "62974453ec4b32f200362339",
+      astro: "62974453ec4b32f200362337",
+      naut: "62974453ec4b32f200362338",
+    },
+  },
+];
+
+module.exports = {
+  mockAstros,
+};
