2d6fac8bc78c71602853beb05a19c536bf7e4be5
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-03 13:30:23 +0200

    Adding .delete at /:idAstro at astroRouters.js

--- src/server/routers/astroRouter.js
+++ src/server/routers/astroRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAstros } = require("../controllers/astroController");
+const { getAstros, deleteAstro } = require("../controllers/astroController");
 
 const astroRouter = express.Router();
 
 astroRouter.get("/", getAstros);
+astroRouter.delete("/:idAstro", deleteAstro);
 
 module.exports = astroRouter;
