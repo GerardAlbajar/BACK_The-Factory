35c6206dadb5b949f5e07f9570fef3c25892979c
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-03 12:29:29 +0200

    Adding .delete at /:idAstroPart at astropartRouters.js

--- src/server/routers/astropartRouters.js
+++ src/server/routers/astropartRouters.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getAstroParts } = require("../controllers/astroPartController");
+const {
+  getAstroParts,
+  deleteAstroPart,
+} = require("../controllers/astroPartController");
 
 const astroPartRouter = express.Router();
 
 astroPartRouter.get("/", getAstroParts);
+astroPartRouter.delete("/:idAstroPart", deleteAstroPart);
 
 module.exports = astroPartRouter;
