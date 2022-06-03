a3e9a66071444faa725a8359b089d0162d5cf327
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-03 12:28:03 +0200

    Adding mockAstroPart at moks/astroParts.js

--- src/mocks/astroParts.js
+++ src/mocks/astroParts.js
@@ -25,6 +25,22 @@ const mockAstroParts = [
   },
 ];
 
+const mockAstroPart = [
+  {
+    id: "#812736SJDGHA",
+    name: "REACT ASTRO",
+    type: "Astro",
+    framework: "React",
+    assembled: "false",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/astro-factory.appspot.com/o/REACT_ASTRO.png?alt=media&token=8934e3f3-8e42-4ddd-8dec-9fe74acc6c47",
+    partimage:
+      "https://firebasestorage.googleapis.com/v0/b/astro-factory.appspot.com/o/REACT_ASTRO_2.png?alt=media&token=7e1ef3af-b744-4d81-9c44-20346c04977e",
+    owner: "6294eeb64326e4549274b515",
+  },
+];
+
 module.exports = {
   mockAstroParts,
+  mockAstroPart,
 };
