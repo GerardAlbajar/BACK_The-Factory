fafd541ad20a3886c4c99c65ce7ff33b062fbbca
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-06 18:59:45 +0200

    Adding /:idUser/:inventoryKey/:idItemToRemove endpoint at /inventory

--- src/server/routers/inventoryRouter.js
+++ src/server/routers/inventoryRouter.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { getInventory } = require("../controllers/inventoryController");
+const {
+  getInventory,
+  deleteInventoryItem,
+} = require("../controllers/inventoryController");
 
 const inventoryRouter = express.Router();
 
 inventoryRouter.get("/:idUser", getInventory);
+inventoryRouter.delete(
+  "/:idUser/:inventoryKey/:idItemToRemove",
+  deleteInventoryItem
+);
 
 module.exports = inventoryRouter;
