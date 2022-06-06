0a754bae03fd5e9bc05bf4d2bd5be0762d157a33
Gerard Albajar <gerard.albajar@gmail.com>
2022-06-06 18:58:40 +0200

    Adding deleteInventoryItem at inventoryController.js

--- src/server/controllers/inventoryController.test.js
+++ src/server/controllers/inventoryController.test.js
@@ -1,7 +1,18 @@
 const User = require("../../database/models/User");
-const { getInventory } = require("./inventoryController");
+// const { mockAstroParts, mockAstroPart } = require("../../mocks/astroParts");
+const {
+  getInventory,
+  // deleteInventoryAstroPart,
+  // deleteInventoryAstro,
+} = require("./inventoryController");
 
 const next = jest.fn();
+
+// jest.mock("../../database/models/AstroPart", () => ({
+//   ...jest.requireActual("../../database/models/AstroPart"),
+//   find: jest.fn().mockResolvedValue(mockAstroParts),
+//   findByIdAndDelete: jest.fn().mockResolvedValue(mockAstroPart),
+// }));
 
 describe("Given a getInventory function", () => {
   const req = {
@@ -19,3 +30,67 @@ describe("Given a getInventory function", () => {
     });
   });
 });
+
+// describe("Given a deleteInventoryAstroPart function", () => {
+//   describe("When it's invoked with a response and an Astro Part with id 1", () => {
+//     const req = {
+//       params: {
+//         idInventoryAstroPart: 1,
+//       },
+//     };
+
+//     const res = {
+//       status: jest.fn().mockReturnThis(),
+//       json: jest.fn(),
+//     };
+
+//     test("Then it should call the response status method with 200", async () => {
+//       const expectedStatus = 200;
+
+//       await deleteInventoryAstroPart(req, res);
+
+//       expect(res.status).toHaveBeenCalledWith(expectedStatus);
+//     });
+
+//     test("Then it should call the response json method with 'Your Astro Part has been deleted (Astro Part number 1)' message", () => {
+//       deleteInventoryAstroPart(req, res);
+//       const expectedMessage = {
+//         msg: "Your Astro Part has been deleted (Astro Part number 1)",
+//       };
+
+//       expect(res.json).toHaveBeenCalledWith(expectedMessage);
+//     });
+//   });
+// });
+
+// describe("Given a deleteInventoryAstro function", () => {
+//   describe("When it's invoked with a response and an Astro Part with id 1", () => {
+//     const req = {
+//       params: {
+//         idInventoryAstro: 1,
+//       },
+//     };
+
+//     const res = {
+//       status: jest.fn().mockReturnThis(),
+//       json: jest.fn(),
+//     };
+
+//     test("Then it should call the response status method with 200", async () => {
+//       const expectedStatus = 200;
+
+//       await deleteInventoryAstro(req, res);
+
+//       expect(res.status).toHaveBeenCalledWith(expectedStatus);
+//     });
+
+//     test("Then it should call the response json method with 'Your Astro Part has been deleted (Astro Part number 1)' message", () => {
+//       deleteInventoryAstro(req, res);
+//       const expectedMessage = {
+//         msg: "Your Astro Part has been deleted (Astro number 1)",
+//       };
+
+//       expect(res.json).toHaveBeenCalledWith(expectedMessage);
+//     });
+//   });
+// });
